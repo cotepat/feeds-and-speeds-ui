95850fdd5f78af13f364098cd065cd177cbbaffe
Adam Costanza <adamco@zillowgroup.com>
2022-02-04 16:07:49 -0700

    katex

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import VueKatex from 'vue-katex';
-import 'katex/dist/katex.min.css';
+import VueKatex from "vue-katex";
+import "katex/dist/katex.min.css";
 Vue.config.productionTip = false;
+
+Vue.use(VueKatex);
 
 new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <div>{{equation}}</div>
-    <div>hi</div>
+    <div v-katex="equation"></div>
+    <div>hi test</div>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
@@ -10,17 +10,18 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import * as nerdamer from 'nerdamer'
+import * as nerdamer from "nerdamer";
+
 @Component({
   components: {
     HelloWorld,
   },
 })
 export default class Home extends Vue {
-  equation = ""
+  equation = "";
   created() {
-    this.equation = nerdamer.convertToLaTeX('x^2+2*(cos(x)+x*x)')
-    console.log(this.equation)
+    this.equation = nerdamer.convertToLaTeX("x^2+2*(cos(x)+x*x)");
+    console.log(this.equation);
   }
 }
 </script>
