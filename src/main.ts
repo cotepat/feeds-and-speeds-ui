c533fcf3f6527d25409510bad0a1b4a44582081d
adcostanza <adcostanza@gmail.com>
2022-02-04 10:03:33 -0700

    WIP

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import VueKatex from 'vue-katex';
+import 'katex/dist/katex.min.css';
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <div>{{equation}}</div>
+    <div>hi</div>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
@@ -8,11 +10,17 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import * as nerdamer from 'nerdamer'
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  equation = ""
+  created() {
+    this.equation = nerdamer.convertToLaTeX('x^2+2*(cos(x)+x*x)')
+    console.log(this.equation)
+  }
+}
 </script>
